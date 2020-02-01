1bab57bce1222ea2184f0c61037b73b227911110
dunkman32 <dunkman032@gmail.com>
2020-02-02 01:30:19 +0400

    added another registration, added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import {PdfComponent} from './pdf/pdf.component';
 import {RegistrationV2Component} from './registration-v2/registration-v2.component';
 import {AuthComponent} from './auth/auth.component';
 import {LikeRsComponent} from './like-rs/like-rs.component';
-import { AnotherRegistrationComponent } from './another-registration/another-registration.component';
+import {AnotherRegistrationComponent} from './another-registration/another-registration.component';
 
 
 const routes: Routes = [
-  {path: '', component: AnotherRegistrationComponent},
+  {path: '', component: RegistrationV2Component},
   {path: 'registration', component: RegistrationV2Component},
   {path: 'another-registration', component: AnotherRegistrationComponent},
   {path: 'pdf', component: PdfComponent},
